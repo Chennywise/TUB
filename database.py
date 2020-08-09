a09b887e151e7aeba93a3360f32e5e3479b78537
BCheng-KH <67608928+BCheng-KH@users.noreply.github.com>
2020-08-08 20:00:39 -0400

    Update database.py

--- database.py
+++ database.py
@@ -33,8 +33,8 @@ class DataBase:
         if self.get_user(email) == -1:
             command = "INSERT INTO users (username, password, email, creation_date) VALUES(%s, %s, %s, %s)"
             values = (name.strip(), password.strip(), email.strip(), DataBase.get_date())
-            self.mycursor.execute(command, values)
             self.mydb.commit()
+            self.mycursor.execute(command, values)
             return 1
 
 
@@ -43,8 +43,12 @@ class DataBase:
             return -1
     def add_club(self, name, school, description, status):
         command = "INSERT INTO club_list (club_name, club_school, descr, stat, creation_date) VALUES(%s, %s, %s, %s, %s)"
-        val = (name.strip(), school.strip().lower(), descr.strip(), status, DataBase.get_date())
+        val = (name.strip(), school.strip().lower(), description.strip(), status, DataBase.get_date())
         self.mycursor.execute(command, val)
+        self.mycursor.execute("SELECT LAST_INSERT_ID()")
+        id = self.mycursor.fetchone()[0]
+        print(id)
+        self.joinClub(id, admin = True)
         self.mydb.commit()
 
     def validate(self, email, password):
@@ -75,9 +79,40 @@ class DataBase:
         self.mycursor.execute(command)
         self.mydb.commit()
     def searchClubs(self, school, keyword):
-        command = "SELECT club_name, club_school FROM club_list WHERE club_school LIKE \'%"+school+"%\' AND club_name LIKE \'%"+keyword+"%\'"
+        command = "SELECT club_name, club_school, club_id FROM club_list WHERE club_school LIKE \'%"+school+"%\' AND club_name LIKE \'%"+keyword+"%\' ORDER BY club_name"
         self.mycursor.execute(command)
         return self.mycursor.fetchall()
+    def searchMyClubs(self, school, keyword):
+        command = "SELECT c.club_name, c.club_school, c.club_id FROM club_list c RIGHT JOIN members m ON c.club_id = m.club_id WHERE c.club_school LIKE \'%"+school+"%\' AND c.club_name LIKE \'%"+keyword+"%\' ORDER BY c.club_name"
+        self.mycursor.execute(command)
+        return self.mycursor.fetchall()
+    def joinClub(self, club_id, admin = False):
+        command = "INSERT INTO club_members (club_id, user_id, role, `rank`, admin_stat) VALUES(%s, %s, %s, %s, %s)"
+        val = (club_id, self.user_id, "member", 0, admin)
+        self.mycursor.execute(command, val)
+        self.mydb.commit()
+
+    def getClubPage(self, club_id):
+        command = "SELECT club_name, club_school, descrn, creation_date, stat FROM club_list WHERE club_id = " + str(club_id)
+        self.mycursor.execute(command)
+        return self.mycursor.fetchone()
+    def isInClub(self, club_id):
+        command = "SELECT admin_stat FROM club_members WHERE user_id = {} AND club_id = {}".format(self.user_id, club_id)
+        self.mycursor.execute(command)
+        result = self.mycursor.fetchall()
+        if len(result) != 0:
+            if result[0][0] == 1:
+                return "admin"
+            else:
+                return "in"
+        else:
+            command = "SELECT application_id FROM club_applications WHERE user_id = {} AND club_id = {}".format(self.user_id, club_id)
+            self.mycursor.execute(command)
+            if len(self.mycursor.fetchall()) != 0:
+                return "pending"
+            else:
+                return "not in"
+            
 
     @staticmethod
     def get_date():
